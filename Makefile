4f995c7cccf21b759258ece946c5e82afe4be38e
ilikepepsi <il1k3pepsi@gmail.com>
2016-01-04 11:25:55 +0100

    Added script to set bootargs in uEnv.txt automatically

--- Makefile
+++ Makefile
@@ -1,23 +1,31 @@
-i2c1:
+i2c1_build:
 	cd i2c1/ ;\
-	make blob ;\
+	make build ;
+
+i2c1_install:
+	cd i2c1/ ;\
 	make install ;
 
-i2c1_remove:
+i2c1_uninstall:
 	cd i2c1/ ;\
 	make uninstall ;
 
-spi0:
-	cd spi0/ ;\
-	make blob ;\
-	make install ;
-
-spi0_remove:
-	cd spi0/ ;\
-	make uninstall ;
-
-clean:
+i2c1_clean:
 	cd i2c1/ ;\
 	make clean;
+
+spi0_build:
+	cd spi0/ ;\
+	make build ;
+
+spi0_install:
+	cd spi0/ ;\
+	make install
+
+spi0_uninstall:
+	cd spi0/ ;\
+	make uninstall ;
+
+spi0_clean:
 	cd spi0/ ;\
 	make clean;
---
+++ scripts/bootargs.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+__author__ = "archuser"
+
+import argparse
+import sys
+
+from pprint import pprint as pp
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help = "file to operate on")
+parser.add_argument("--select", help = "show a keys associated value string (if present)")
+parser.add_argument("--append", help = "append input string to value of selected key")
+parser.add_argument("--remove", help = "remove input string from value of selected key")
+parser.add_argument("--add", help = "add a new key/value parameter")
+
+#parser.add_argument("key", help = "the key whose value you want to modify")
+
+args = parser.parse_args()
+
+
+def deserialize(filename):
+    with open(args.file, mode='r') as f:
+        d = {}
+        for line in f.readlines():
+            values = line.split("=", 1)
+            if len(values) < 2:
+                continue
+            d[values[0].strip()] = values[1].strip()
+
+    return d
+
+
+def serialize(d):
+    result = []
+    for key, value in d.items():
+        result.append(key + " = " + value + "\n")
+
+    with open(args.file, mode='w') as f:
+        result.sort()
+        f.writelines(result)
+
+
+def main():
+    maintain_default = True
+
+    d = deserialize(args.file)
+
+    if args.select:
+        maintain_default = False
+
+        if args.append:
+            d[args.select] = d[args.select] + " " + args.append.strip()
+
+        if args.remove:
+            d[args.select] = d[args.select].replace(args.remove, "").strip()
+
+        if args.select in d:
+            print("{0} = {1}".format(args.select, d.get(args.select)))
+        else:
+            sys.exit("key '{0}' not found in {1}".format(args.show, args.file))
+            return
+
+    elif args.add:
+        result = args.add.split("=", 1)
+        d[result[0]] = result[1]
+
+    elif args.remove:
+        d.pop(args.remove)
+
+    else:
+        # pretty print the dict
+        pp(d)
+        return
+
+    serialize(d)
+
+if __name__ == '__main__':
+    main()
